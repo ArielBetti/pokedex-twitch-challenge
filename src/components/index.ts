ed05ad63142feb80c80060b1f3e166544127e082
Ariel Betti <ariel.betti@gmail.com>
2023-01-21 04:20:50 -0300

    feat: import all visual components in module

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import Button from "./Button";
+import InlineLoading from "./InlineLoading";
+import Input from "./Input";
+import List from "./List";
+import LoadingStatus from "./LoadingStatus";
+import PokeCard from "./PokeCard";
+import ThemeToggle from "./ThemeToggle";
+
+export {
+  Button,
+  InlineLoading,
+  Input,
+  List,
+  LoadingStatus,
+  PokeCard,
+  ThemeToggle,
+};
